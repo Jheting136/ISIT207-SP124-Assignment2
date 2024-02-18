6ad0ffc4b7d497fe06af1c1779e72528ebb897c1
Sarah <sarahrahloh@gmail.com>
2024-02-18 13:40:53 +0800

    implemented create account modal

--- Views/Templates/createAccountModal.js
+++ Views/Templates/createAccountModal.js
@@ -8,7 +8,7 @@ function displayCreateAccModal() {
 
 
     createAccModal.innerHTML = `
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="loginModalLabel">Create My Account</h5>
@@ -17,10 +17,22 @@ function displayCreateAccModal() {
                     <div class="modal-body">
                         <!-- Add your login form or content here -->
                         <form>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="Username" name="username">
+
+                            <div  class="grid-container-3">
+                                <div class="grid-item">
+                                    <label for="name" class="form-label">First Name</label>
+                                    <input type="text" class="form-control" id="name" name="name">
+                                </div>
+                                <div class="grid-item">
+                                    <label for="lastName" class="form-label">Last Name</label>
+                                    <input type="text" class="form-control" id="lastName" name="lastName">
+                                </div>
+                                <div class="grid-item">
+                                    <label for="age" class="form-label">Age</label>
+                                    <input type="number" class="form-control" id="age" name="age">
+                                </div>
                             </div>
+
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
                                 <input type="text" class="form-control" id="email" name="Email">
@@ -29,14 +41,44 @@ function displayCreateAccModal() {
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" class="form-control" id="password" name="password">
                             </div>
-                            <div  class="grid-container">
-                                <div class="grid-item" data-bs-dismiss="modal"><button type="button" class="btn btn-primary" onclick="validateLogin(); updateNavLink(); updateReserveBtn(); closeLoginModal(); return false;">Let&#39;s get started</button></div>
-                                <div class="grid-item" style="display: flex; justify-content: flex-end;"><a href="#" class="nav-link"><small data-bs-toggle="modal" data-bs-target="#createAcc" data-bs-dismiss="modal" style="color:grey">I dont have an account</small></a></div>
+                            <hr>
+                            <h5>Card Details</h5>
+                            <div class="mb-3">
+                                <label for="NOC" class="form-label">Name On Card</label>
+                                <input type="text" class="form-control" id="NOC" name="NOC">
                             </div>
-                            <div  class="grid-container">
-                                <div class="grid-item" data-bs-dismiss="modal"><button type="button" class="btn btn-primary" onclick="validateLogin(); updateNavLink(); updateReserveBtn(); closeLoginModal(); return false;">Let&#39;s get started</button></div>
-                                <div class="grid-item" style="display: flex; justify-content: flex-end;"><a href="#" class="nav-link"><small data-bs-toggle="modal" data-bs-target="#createAcc" data-bs-dismiss="modal" style="color:grey">I dont have an account</small></a></div>
+                            <div class="grid-container">
+                                <div class="mb-3">
+                                    <label for="cardNo" class="form-label">Card Number</label>
+                                    <input type="text" class="form-control" id="cardNo" name="cardNo">
+                                </div>
+                                <div class="grid-container">
+                                    <div class="mb-3">
+                                        <label for="cvv" class="form-label">CVV</label>
+                                        <input style="width: 4vw; padding-right: 1vw" type="text" class="form-control" id="cvv" name="cvv">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exp" class="form-label">Card Expiry </label>
+                                        <input type="month" class="form-control" id="exp" name="exp">
+                                    </div>
+                                </div>
                             </div>
+                            <div  class="grid-container-3">
+                                <div class="grid-item">
+                                    <label for="country" class="form-label">Country</label>
+                                    <input type="text" class="form-control" id="country" name="country">
+                                </div>
+                                <div class="grid-item">
+                                    <label for="address" class="form-label">Address</label>
+                                    <input type="text" class="form-control" id="address" name="address">
+                                </div>
+                                <div class="grid-item">
+                                    <label for="postalCode" class="form-label">Postal Code</label>
+                                    <input type="text" class="form-control" id="postalCode" name="postalCode">
+                                </div>
+                            </div>
+                            <div class="grid-item" data-bs-dismiss="modal"><button type="button" class="btn btn-primary" onclick="closeLoginModal(); return false;">Let&#39;s get started</button></div>
+
 
                         </form>
                     </div>
